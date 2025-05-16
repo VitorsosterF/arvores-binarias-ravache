1710f0bc5d19d51a66cdc21533e9174d10e5803e
VitorsosterF <vitor.soster.f@gmail.com>
2025-05-15 22:00:00 -0300

    Implementação básica nó e árvore

---
+++ src/No.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.Node;
+
+public class No
+{
+    int valor;
+    Node esquerdo, direito;
+
+    public No(int item)
+    {
+        valor = item;
+        esquerdo = direito = null;
+    }
+
+}
